0593b120d7e2acc524a19fcf3ffcfc6e08423a73
Cader <mcader@hasnatech.com>
2019-10-22 15:02:29 +0530

    Updates

--- main.js
+++ main.js
@@ -44,6 +44,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _pages_product_product_component__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ./pages/product/product.component */ "./src/app/pages/product/product.component.ts");
 /* harmony import */ var _pages_product_form_product_form_component__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ./pages/product-form/product-form.component */ "./src/app/pages/product-form/product-form.component.ts");
 /* harmony import */ var _pages_product_list_product_list_component__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! ./pages/product-list/product-list.component */ "./src/app/pages/product-list/product-list.component.ts");
+/* harmony import */ var _pages_outbound_form_outbound_form_component__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__(/*! ./pages/outbound-form/outbound-form.component */ "./src/app/pages/outbound-form/outbound-form.component.ts");
+
 
 
 
@@ -72,10 +74,6 @@ var routes = [{
         component: _pages_inbound_new_inbound_new_component__WEBPACK_IMPORTED_MODULE_6__["InboundNewComponent"]
     },
     {
-        path: "outbound/form",
-        component: _pages_inbound_new_inbound_new_component__WEBPACK_IMPORTED_MODULE_6__["InboundNewComponent"]
-    },
-    {
         path: "product/list",
         component: _pages_product_list_product_list_component__WEBPACK_IMPORTED_MODULE_10__["ProductListComponent"]
     },
@@ -98,6 +96,10 @@ var routes = [{
     {
         path: "outbound",
         component: _pages_outbound_outbound_component__WEBPACK_IMPORTED_MODULE_4__["OutboundComponent"]
+    },
+    {
+        path: "outbound/form",
+        component: _pages_outbound_form_outbound_form_component__WEBPACK_IMPORTED_MODULE_11__["OutboundFormComponent"]
     }];
 var AppRoutingModule = /** @class */ (function () {
     function AppRoutingModule() {
@@ -193,6 +195,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _pages_scan_scan_component__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__(/*! ./pages/scan/scan.component */ "./src/app/pages/scan/scan.component.ts");
 /* harmony import */ var _pages_product_form_product_form_component__WEBPACK_IMPORTED_MODULE_12__ = __webpack_require__(/*! ./pages/product-form/product-form.component */ "./src/app/pages/product-form/product-form.component.ts");
 /* harmony import */ var _pages_product_list_product_list_component__WEBPACK_IMPORTED_MODULE_13__ = __webpack_require__(/*! ./pages/product-list/product-list.component */ "./src/app/pages/product-list/product-list.component.ts");
+/* harmony import */ var _pages_outbound_form_outbound_form_component__WEBPACK_IMPORTED_MODULE_14__ = __webpack_require__(/*! ./pages/outbound-form/outbound-form.component */ "./src/app/pages/outbound-form/outbound-form.component.ts");
+
 
 
 
@@ -222,7 +226,8 @@ var AppModule = /** @class */ (function () {
                 _pages_product_product_component__WEBPACK_IMPORTED_MODULE_10__["ProductComponent"],
                 _pages_scan_scan_component__WEBPACK_IMPORTED_MODULE_11__["ScanComponent"],
                 _pages_product_form_product_form_component__WEBPACK_IMPORTED_MODULE_12__["ProductFormComponent"],
-                _pages_product_list_product_list_component__WEBPACK_IMPORTED_MODULE_13__["ProductListComponent"]
+                _pages_product_list_product_list_component__WEBPACK_IMPORTED_MODULE_13__["ProductListComponent"],
+                _pages_outbound_form_outbound_form_component__WEBPACK_IMPORTED_MODULE_14__["OutboundFormComponent"]
             ],
             imports: [
                 _angular_platform_browser__WEBPACK_IMPORTED_MODULE_1__["BrowserModule"],
@@ -475,6 +480,75 @@ var InboundComponent = /** @class */ (function () {
         tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:paramtypes", [src_app_service_common_service__WEBPACK_IMPORTED_MODULE_2__["CommonService"]])
     ], InboundComponent);
     return InboundComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/pages/outbound-form/outbound-form.component.html":
+/*!******************************************************************!*\
+  !*** ./src/app/pages/outbound-form/outbound-form.component.html ***!
+  \******************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"container\">\n\n  <div class=\"row\">\n    <div class=\"col-sm-2 border-right\">\n      <h5>NON HAZARDOUS WASTE MAINFEST</h5>\n    </div>\n    <div class=\"col-sm-2 border-right\">\n      <p>1. Generator ID Number</p>\n      <input type=\"text\" class=\"form-control\" value=\"CAL000092413\">\n    </div>\n    <div class=\"col-sm-2 border-right\">\n      <p>page 1 of</p>\n      <input type=\"text\" class=\"form-control\" value=\"1\">\n    </div>\n    <div class=\"col-sm-3 border-right\">\n      <p>3.Emaegency Response Phone</p>\n      <input type=\"text\" class=\"form-control\" value=\"(800) 424-9300\">\n    </div>\n    <div class=\"col-sm-3\">\n      <p>4.Manifest Tracking Number</p>\n      <input type=\"text\" class=\"form-control\" value=\"020376270  JJK\">\n    </div>\n\n  </div>\n\n\n\n\n\n  <div class=\"row\">\n    <div class=\"col-sm-6\">\n      <p>Generator Name and Mailing Address</p>\n      <textarea class=\"form-control\" row=\"3\"\n        cols=\"50\">IOLCHANGERS &#13;&#10;198 VALACIA ST SAN FRANCISCO CA&#13;&#10;94103</textarea>\n      <p>Generator's Phone <input type=\"text\" class=\"form-control\" value=\"415 487 1740\"></p>\n    </div>\n    <div class=\"col-sm-6\">\n      <p>Generator's site Address(if different than mailing address</p>\n    </div>\n  </div>\n\n\n  <div class=\"row\">\n    <div class=col-sm-9>\n      <p>6.Transporter 1 Company Name</p>\n      <input type=\"text\" class=\"form-control\" value=\"WORLD OIL ENVIRONMENTAL SERVICE\">\n    </div>\n    <div class=col-sm-3>\n      <p>U.S EPA ID Number</p>\n      <input type=\"text\" class=\"form-control\" value=\"CAD028277036\">\n    </div>\n  </div>\n\n\n\n  <div class=\"row\">\n    <div class=col-sm-9>\n      <p>7.Transporter 2 Company Name</p>\n      <input type=\"text\" class=\"form-control\" value=\"WORLDWIDE RECOVERY SYSTEM INC\">\n    </div>\n    <div class=col-sm-3>\n      <p>U.S EPA ID Number</p>\n      <input type=\"text\" class=\"form-control\" value=\"CAR000175422\">\n    </div>\n  </div>\n\n\n\n  <div class=\"row\">\n    <div class=col-sm-9>\n      <p>8.Designated Facility Name and Site Address</p>\n      <textarea class=\"form-control\" row=\"3\"\n        cols=\"50\">YES MANAGEMENT INC &#13;&#10;6500 US HIGHWAY 95&#13;&#10;YUMA              AZ       85365</textarea>\n      <p>Facility's Phone</p>\n      <input type=\"text\" class=\"form-control\" value=\"(928)344-9828\">\n\n    </div>\n    <div class=col-sm-3>\n      <p>U.S EPA ID Number</p>\n      <input type=\"text\" class=\"form-control\" value=\"AZR000521146\">\n    </div>\n  </div>\n\n  <div class=\"row\">\n    <table class=\"table table-bordered\">\n      <tr>\n        <th>9a.HM</th>\n        <th>9b. U.S DOT Description(including proper shipping Name,Hazard Class,ID Number, and Packing Group(if any))\n        </th>\n        <th colspan=\"2\">10. Containers</th>\n        <th>11.Total Quantity</th>\n        <th>12 Unit Wt/Vol</th>\n        <th>13.Waste Codes</th>\n      </tr>\n      <tr>\n        <td></td>\n        <td></td>\n        <td>NO.</td>\n        <td>Type</td>\n        <td></td>\n        <td></td>\n        <td></td>\n      </tr>\n      <tr *ngFor=\"let item of products; let i = index;\">\n        <td>{{i + 1}}</td>\n        <td>{{item.name}}</td>\n        <td>{{item.container_no}}</td>\n        <td>{{item.container_type}}</td>\n        <td>{{item.quantity}}</td>\n        <td></td>\n        <td>{{item.waste_code}}</td>\n        <td></td>\n      </tr>\n\n    </table>\n    <button class=\"btn btn-primary\" routerLink=\"/scan/outbound\">Scan</button>\n  </div>\n\n\n\n  <div class=\"row\">\n    <div class=\"col-sm-12\">\n      <p>14.Special Handling Instructions and Additional Information</p>\n      <h4>EMERGENCY CONTACT : CHEMTREC 1-800-424-9300 WHOSE TERMINAL NAERG# 9B1 : 171 *PROFILE# 9B1 : 162623NRS<br>OILY\n        DEBRIS * APPROPRIATE PERSONAL PROTECTIVE EQUIPMENT</h4>\n      <div class=\"row\">\n        <div class=\"col-sm-3\"></div>\n        <div class=\"col-sm-3\"><input type=\"text\" class=\"form-control\" value=\"TRUCK 2030\"></div>\n        <div class=\"col-sm-3\"><input type=\"text\" class=\"form-control\" value=\"1-559 PM\"></div>\n        <div class=\"col-sm-3\"><input type=\"text\" class=\"form-control\" value=\"WO#543295\"></div>\n      </div>\n    </div>\n  </div>\n\n  <div class=\"row\">\n    <div class=\"col-sm-12\">\n      <div>\n        <h5 style='display:inline'>GENERATOR'S/OFFERORS CERTIFICATION :</h5>\n        <p style='display:inline'>I hereby declare that the content of this cosigment are fully and accurately described\n          above by the proper shipping name,and are classified,packeged,marketed and labeled/placarded and are in all\n          respect in proper codition for transport According to applicable International and national govermental\n          regulations.if export shipment and\n          I am the primary Exporter, i certify that the contents of this consigment conform to the terms of the attached\n          EPA acknow;edgement of Consent<br>I certify that the waste minimization statement identified in 40CFR 262.27\n          (a)(if i am a large quantity Generator) or (b)(if i am a small quantity Generator) is true.</p>\n      </div>\n    </div>\n  </div>\n\n  <div class=\"row\">\n    <div class=\"col-sm-6\">\n      <p>Generator's/Officer's printed/Type Name </p>\n      <input type=\"text\" class=\"form-control\" value=\"Jonte\">\n\n    </div>\n    <div class=\"col-sm-3\">\n      <p>Signature</p>\n\n\n    </div>\n    <div class=\"col-sm-3\">\n      <p>Date</p>\n      <input type=\"text\" class=\"form-control\">\n    </div>\n\n  </div>\n\n\n\n  <div class=\"row\">\n    <div class=\"col-sm-3\">\n      <p>International Shipments</p>\n      <p>Transporter Signature (for exports only): </p>\n    </div>\n    <div class=\"col-sm-2\">\n      <p> <input type=\"checkbox\">Import to U.S</p>\n\n    </div>\n    <div class=\"col-sm-2\">\n      <p> <input type=\"checkbox\">Export from U.S</p>\n    </div>\n    <div class=\"col-sm-5\">\n      <p>Port of entry/exit:_________________________________</p>\n      <p>Dateleaving U.S:</p>\n    </div>\n  </div>\n\n\n\n\n\n  <div class=\"row\">\n    <div class=\"col-sm-12\">\n      <p>17.Transporter Acknowledgement of Receipt of Materials</p>\n    </div>\n  </div>\n\n\n  <div class=\"row\">\n    <div class=\"col-sm-5\">\n      <p>Transporter 1 Printed/Typed Name</p>\n      <input type=\"text\" class=\"form-control\" value=\"RAWN GANGA\">\n    </div>\n    <div class=\"col-sm-4\">\n      <p>Signature</p>\n\n    </div>\n    <div class=\"col-sm-3\">\n      <p>Date</p>\n      <input type=\"text\" class=\"form-control\">\n    </div>\n  </div>\n\n\n\n\n  <div class=\"row\">\n    <div class=\"col-sm-5\">\n      <p>Transporter 2 Printed/Typed Name</p>\n      <input type=\"text\" class=\"form-control\" value=\"Paul D Mic Tarcsno Jk\">\n    </div>\n    <div class=\"col-sm-4\">\n      <p>Signature</p>\n\n    </div>\n    <div class=\"col-sm-3\">\n      <p>Date</p>\n      <input type=\"text\" class=\"form-control\">\n    </div>\n\n  </div>\n\n\n\n  <div class=\"row\">\n    <div class=\"col-sm-12\">\n      <p>18.Discrepancy</p>\n    </div>\n  </div>\n\n\n\n\n\n</div>\n"
+
+/***/ }),
+
+/***/ "./src/app/pages/outbound-form/outbound-form.component.scss":
+/*!******************************************************************!*\
+  !*** ./src/app/pages/outbound-form/outbound-form.component.scss ***!
+  \******************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = ".row {\n  padding: 10px 0px;\n  border-bottom: 1px solid #ccc; }\n\n.container {\n  border: 1px solid #ccc; }\n\n.table {\n  margin-bottom: 0rem; }\n\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNyYy9hcHAvcGFnZXMvb3V0Ym91bmQtZm9ybS9GOlxcR0lUXFxQcm9qZWN0XFxCcmFkXFxCYXJjb2RlLU5nL3NyY1xcYXBwXFxwYWdlc1xcb3V0Ym91bmQtZm9ybVxcb3V0Ym91bmQtZm9ybS5jb21wb25lbnQuc2NzcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtFQUNJLGlCQUFpQjtFQUNqQiw2QkFBNkIsRUFBQTs7QUFFakM7RUFDSSxzQkFBc0IsRUFBQTs7QUFFMUI7RUFFSSxtQkFBbUIsRUFBQSIsImZpbGUiOiJzcmMvYXBwL3BhZ2VzL291dGJvdW5kLWZvcm0vb3V0Ym91bmQtZm9ybS5jb21wb25lbnQuc2NzcyIsInNvdXJjZXNDb250ZW50IjpbIi5yb3d7XHJcbiAgICBwYWRkaW5nOiAxMHB4IDBweDtcclxuICAgIGJvcmRlci1ib3R0b206IDFweCBzb2xpZCAjY2NjO1xyXG59XHJcbi5jb250YWluZXJ7XHJcbiAgICBib3JkZXI6IDFweCBzb2xpZCAjY2NjO1xyXG59XHJcbi50YWJsZSB7XHJcbiAgICBcclxuICAgIG1hcmdpbi1ib3R0b206IDByZW07XHJcbn0iXX0= */"
+
+/***/ }),
+
+/***/ "./src/app/pages/outbound-form/outbound-form.component.ts":
+/*!****************************************************************!*\
+  !*** ./src/app/pages/outbound-form/outbound-form.component.ts ***!
+  \****************************************************************/
+/*! exports provided: OutboundFormComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "OutboundFormComponent", function() { return OutboundFormComponent; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var src_app_service_common_service__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! src/app/service/common.service */ "./src/app/service/common.service.ts");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm5/router.js");
+
+
+
+
+var OutboundFormComponent = /** @class */ (function () {
+    function OutboundFormComponent(service, route) {
+        this.service = service;
+        this.route = route;
+        this.products = [];
+    }
+    OutboundFormComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        this.route.queryParams.subscribe(function (data) {
+            if (data.product != null) {
+                _this.products.push(_this.service.getProduct(data.product));
+            }
+        });
+    };
+    OutboundFormComponent = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
+            selector: 'app-outbound-form',
+            template: __webpack_require__(/*! ./outbound-form.component.html */ "./src/app/pages/outbound-form/outbound-form.component.html"),
+            styles: [__webpack_require__(/*! ./outbound-form.component.scss */ "./src/app/pages/outbound-form/outbound-form.component.scss")]
+        }),
+        tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:paramtypes", [src_app_service_common_service__WEBPACK_IMPORTED_MODULE_2__["CommonService"], _angular_router__WEBPACK_IMPORTED_MODULE_3__["ActivatedRoute"]])
+    ], OutboundFormComponent);
+    return OutboundFormComponent;
 }());
 
 
@@ -902,29 +976,29 @@ __webpack_require__.r(__webpack_exports__);
 var CommonService = /** @class */ (function () {
     function CommonService() {
         this.product = [{
-                name: "Non RCRA HAZARDOUS WASTE SOLID",
-                code: "9840033126",
-                container_no: "001",
+                name: "Sutter Home Vineyards Inc",
+                code: "CAL000171777",
+                container_no: "019150099JJK",
                 container_type: "DM",
-                quantity: "100",
+                quantity: "1",
                 unit: "",
-                waste_code: "352"
+                waste_code: "400"
             }, {
-                name: "Product 1",
-                code: "10001",
-                container_no: "001",
-                container_type: "DM",
-                quantity: "50",
+                name: "World Oil Environmental Services",
+                code: "CAD028277036 ",
+                container_no: "CH00109 ",
+                container_type: "DM ",
+                quantity: "71 ",
                 unit: "",
-                waste_code: "154"
+                waste_code: "35000",
             }, {
-                name: "Product 2",
-                code: "45005",
-                container_no: "004",
+                name: "Guidos Auto Precision ",
+                code: "CAL000386161",
+                container_no: "019150080JJK",
                 container_type: "DM",
-                quantity: "70",
+                quantity: "1",
                 unit: "",
-                waste_code: "877"
+                waste_code: "100"
             },];
         //"epa_id_number": "CAD028277036",
         this.inbound = [{
